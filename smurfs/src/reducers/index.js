bfa261952ea344749266f61d40793248bce1f51a
Carlossantana987 <carlossantana987@outlook.com>
2019-06-21 11:44:48 -0400

    smurfs reducer added

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,38 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  SMURFS_FETCH_START,
+  SMURFS_FETCH_SUCCESS,
+  SMURFS_FETCH_FAILURE,
+  SMURFS_ADD_START,
+  SMURFS_ADD_SUCCESS,
+  SMURFS_ADD_FAILURE
+} from "../actions";
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurfs: false,
+  error: null
+};
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SMURFS_FETCH_START:
+      return { ...state, fetchingSmurfs: true };
+
+    case SMURFS_ADD_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: [...state.smurfs, action.payload]
+      };
+
+    case SMURFS_ADD_FAILURE:
+      return { ...state, fetchingSmurfs: false, error: action.payload };
+  }
+};
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -21,3 +53,5 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default smurfReducer;
