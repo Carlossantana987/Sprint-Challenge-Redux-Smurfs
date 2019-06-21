f190ff6ca503c0927c8c602a8dc47d36309015fd
Carlossantana987 <carlossantana987@outlook.com>
2019-06-21 11:32:05 -0400

    smurfs actions created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,39 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
+
+export const SMURFS_FETCH_START = "SMURFS_FETCH_START";
+export const SMURFS_FETCH_SUCCESS = "SMURFS_FETCH_SUCCESS";
+export const SMURFS_FETCH_FAILURE = "SMURFS_FETCH_FAILURE";
+export const SMURFS_ADD_START = "SMURFS_ADD_START";
+export const SMURFS_ADD_SUCCESS = "SMURFS_ADD_SUCCESS";
+export const SMURFS_ADD_FAILURE = "SMURFS_ADD_FAILURE";
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: SMURFS_FETCH_START });
+  axios
+    .get(`http://localhost:3333/smurfs`)
+    .then(res => {
+      dispatch({ type: SMURFS_FETCH_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: SMURFS_FETCH_FAILURE, payload: err });
+    });
+};
+
+export const addSmurfs = newSmurf => dispatch => {
+  dispatch({ type: SMURFS_ADD_START });
+  axios
+    .post(`http://localhost:3333/smurfs`, newSmurf)
+    .then(res => {
+      dispatch({ type: SMURFS_ADD_SUCCESS, payload: res.data });
+    })
+
+    .catch(err => {
+      dispatch({ type: SMURFS_ADD_FAILURE, payload: err });
+    });
+};
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
